b352a6f045924abdc293baaf12a58aee0088671c
KAE <esomokezie604@gmail.com>
2024-03-29 22:22:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="esomokezie604@gmail.com"
 global.location="Lagos,Africa."
 
 
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://darkshan:3000@cluster0.t1wsjlv.mongodb.net/"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
